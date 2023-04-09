6b6d128af8e875847d86e5d9d63053df066289fb
THUSITHANAMAL <89918452+THUSITHANAMAL@users.noreply.github.com>
2023-04-09 15:32:25 +0530

    Add files via upload

---
+++ js/JavaScript.js
@@ -0,0 +1,11 @@
+﻿let menu = document.queryselector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addeventlisener("click", function () {
+    navbar.classlist.toggle("active");
+});
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+}
+
